6876f4f2056e3a55e87057b382a5c7b2e1ec66e4
Tushar <tushar.urade@gmail.com>
2026-05-14 22:14:22 +0530

    gateway configuration uris

--- src/main/java/com/ecommerce/productservice/controller/ProductControlller.java
+++ src/main/java/com/ecommerce/productservice/controller/ProductControlller.java
@@ -7,13 +7,15 @@ import com.ecommerce.productservice.mapper.ProductMapper;
 import com.ecommerce.productservice.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
-@RequestMapping("/api/v1/products")
+@RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductControlller {
     @Autowired
@@ -27,5 +29,11 @@ public class ProductControlller {
     @GetMapping
     public List<Product> getAllProducts() {
         return prodService.getAllProducts();
+    }
+    
+    @GetMapping("/{id}")
+    public String getProduct(@PathVariable Long id) {
+        log.debug("id : {}", id);
+        return "Product " + id;
     }
 }
