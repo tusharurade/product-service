fe61db48902fc948e9da80c535a5829d10c25022
Tushar <tushar.urade@gmail.com>
2026-05-15 00:05:54 +0530

    gateway route filters

--- src/main/java/com/ecommerce/productservice/controller/ProductControlller.java
+++ src/main/java/com/ecommerce/productservice/controller/ProductControlller.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductControlller {
     @Autowired
